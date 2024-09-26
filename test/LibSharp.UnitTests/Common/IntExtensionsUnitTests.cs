1559c8974b4013db2f856234a20fe749aaf58db6
Danylo Fitel <danylo.fitel@outlook.com>
2024-09-25 23:42:10 -0700

    Update tests

--- test/LibSharp.UnitTests/Common/IntExtensionsUnitTests.cs
+++ test/LibSharp.UnitTests/Common/IntExtensionsUnitTests.cs
@@ -1,5 +1,6 @@
 ﻿// Copyright (c) LibSharp. All rights reserved.
 
+using System;
 using System.Net;
 using LibSharp.Common;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
@@ -17,14 +18,19 @@ namespace LibSharp.UnitTests.Common
 
             // Assert
             Assert.IsFalse(int.MinValue.TryConvertToEnum<HttpStatusCode>(out _));
+            Assert.IsFalse((-200).TryConvertToEnum<HttpStatusCode>(out _));
             Assert.IsFalse((-1).TryConvertToEnum<HttpStatusCode>(out _));
             Assert.IsFalse(0.TryConvertToEnum<HttpStatusCode>(out _));
             Assert.IsFalse(1.TryConvertToEnum<HttpStatusCode>(out _));
             Assert.IsFalse(99.TryConvertToEnum<HttpStatusCode>(out _));
+            Assert.IsFalse(600.TryConvertToEnum<HttpStatusCode>(out _));
             Assert.IsFalse(int.MaxValue.TryConvertToEnum<HttpStatusCode>(out _));
 
             Assert.IsTrue(100.TryConvertToEnum<HttpStatusCode>(out result));
             Assert.AreEqual(HttpStatusCode.Continue, result);
+
+            Assert.IsTrue(101.TryConvertToEnum<HttpStatusCode>(out result));
+            Assert.AreEqual(HttpStatusCode.SwitchingProtocols, result);
 
             Assert.IsTrue(200.TryConvertToEnum<HttpStatusCode>(out result));
             Assert.AreEqual(HttpStatusCode.OK, result);
@@ -46,6 +52,39 @@ namespace LibSharp.UnitTests.Common
 
             Assert.IsTrue(505.TryConvertToEnum<HttpStatusCode>(out result));
             Assert.AreEqual(HttpStatusCode.HttpVersionNotSupported, result);
+        }
+
+        [TestMethod]
+        public void TryConvertToEnum_StringComparison()
+        {
+            // Arrange
+            StringComparison result;
+
+            // Assert
+            Assert.IsFalse(int.MinValue.TryConvertToEnum<StringComparison>(out _));
+            Assert.IsFalse((-2).TryConvertToEnum<StringComparison>(out _));
+            Assert.IsFalse((-1).TryConvertToEnum<StringComparison>(out _));
+            Assert.IsFalse(6.TryConvertToEnum<StringComparison>(out _));
+            Assert.IsFalse(7.TryConvertToEnum<StringComparison>(out _));
+            Assert.IsFalse(int.MaxValue.TryConvertToEnum<StringComparison>(out _));
+
+            Assert.IsTrue(0.TryConvertToEnum<StringComparison>(out result));
+            Assert.AreEqual(StringComparison.CurrentCulture, result);
+
+            Assert.IsTrue(1.TryConvertToEnum<StringComparison>(out result));
+            Assert.AreEqual(StringComparison.CurrentCultureIgnoreCase, result);
+
+            Assert.IsTrue(2.TryConvertToEnum<StringComparison>(out result));
+            Assert.AreEqual(StringComparison.InvariantCulture, result);
+
+            Assert.IsTrue(3.TryConvertToEnum<StringComparison>(out result));
+            Assert.AreEqual(StringComparison.InvariantCultureIgnoreCase, result);
+
+            Assert.IsTrue(4.TryConvertToEnum<StringComparison>(out result));
+            Assert.AreEqual(StringComparison.Ordinal, result);
+
+            Assert.IsTrue(5.TryConvertToEnum<StringComparison>(out result));
+            Assert.AreEqual(StringComparison.OrdinalIgnoreCase, result);
         }
     }
 }
